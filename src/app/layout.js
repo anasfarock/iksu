2d59e654ec6fcb081b8eed1beb43ba5a3676053b
anasfarock <anasf1905@gmail.com>
2025-07-12 20:41:09 +0500

    Layout Updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,10 @@
-// src/app/layout.js
-import "./globals.css";
+// layout.js
 import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
-  variable: "--font-poppins",
 });
-
-export const metadata = {
-  title: "IKSU Consultant and Construction Company",
-  description:
-    "Your Ideas & Dreams Are Transformed By Us Into Long-Lasting, Engineered Buildings.",
-};
 
 export default function RootLayout({ children }) {
   return (
